2669df640b74c2a2bfa4e9987a9e18210bdcb83b
Joel Edegran <joel_edegran@hotmail.com>
2026-08-20 14:12:07 +0200

    feat: add Vituum Pug setup and Pico CSS integration

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,15 @@
 import { defineConfig } from 'vite';
-import pug from 'pug';
+import vituum from 'vituum';
+import pug from '@vituum/vite-plugin-pug';
 
 export default defineConfig({
   plugins: [
-    {
-      name: 'vite-plugin-pug-entry',
-      configureServer(server) {
-        server.middlewares.use((req, res, next) => {
-          if (req.url === '/' || req.url === '/index.html') {
-            try {
-              const compiledFunction = pug.compileFile('index.pug');
-              const html = compiledFunction();
-              res.setHeader('Content-Type', 'text/html');
-              return res.end(html);
-            } catch (error) {
-              return next(error);
-            }
-          }
-          next();
-        });
-
-        server.watcher.add('index.pug');
-        server.watcher.on('change', (file) => {
-          if (file.endsWith('index.pug')) {
-            server.ws.send({
-              type: 'full-reload',
-              path: '*'
-            });
-          }
-        });
-      },
-      transformIndexHtml() {
-        const compiledFunction = pug.compileFile('index.pug');
-        return compiledFunction();
-      }
+    vituum(),
+    pug()
+  ],
+  build: {
+    rollupOptions: {
+      input: ['index.pug']
     }
-  ]
+  }
 });
